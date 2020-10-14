326a47fb875e473db69609490b54fc6f8fff4388
Phil Cluff <578330+GeneticGenesis@users.noreply.github.com>
2020-10-14 23:59:14 +0100

    Replace modal css with js because just why

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,11 +2,21 @@
 
   window.onclick = function(event) {
     if (event.target.className == 'modal') {
-      // console.log('Will close modal');
-      // console.log(event.target);
       event.target.style.visibility = 'hidden';
       event.target.style.opacity = '0';
     }
   }
 
 })(document);
+
+function closeModal(event, t) {
+  event.preventDefault();
+  t.closest('.modal').style.visibility = 'hidden';
+  t.closest('.modal').opacity = '0';
+}
+
+function showModal(event, name) {
+  event.preventDefault();
+  document.getElementById(name).style.visibility = 'visible';
+  document.getElementById(name).style.opacity = '1';
+}
