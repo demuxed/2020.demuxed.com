e4c74e45130af0259ed21a2b03e063b53ce9291c
Phil Cluff <578330+GeneticGenesis@users.noreply.github.com>
2020-10-15 00:18:04 +0100

    Cleanup JS

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,21 +1,18 @@
 (function (document) {
-
   window.onclick = function(event) {
     if (event.target.className == 'modal') {
-      event.target.style.visibility = 'hidden';
-      event.target.style.opacity = '0';
+      hideTalkModal(event)
     }
   }
-
 })(document);
 
-function closeModal(event, t) {
+function hideTalkModal(event) {
   event.preventDefault();
-  t.closest('.modal').style.visibility = 'hidden';
-  t.closest('.modal').opacity = '0';
+  event.target.closest('.modal').style.visibility = 'hidden';
+  event.target.closest('.modal').opacity = '0';
 }
 
-function showModal(event, name) {
+function showTalkModal(event, name) {
   event.preventDefault();
   document.getElementById(name).style.visibility = 'visible';
   document.getElementById(name).style.opacity = '1';
