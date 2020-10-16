6025be87cdcf1da911e345effb1ff4a3b5d66a4a
Phil Cluff <578330+GeneticGenesis@users.noreply.github.com>
2020-10-16 12:10:03 +0100

    Handle escape key to close modals

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,17 +1,31 @@
 (function (document) {
+  // Close the open modal when clicked outside
   window.onclick = function(event) {
     if (event.target.className == 'modal') {
       hideTalkModal(event)
     }
   }
+
+  // Close all modals on escape key
+  document.onkeydown = function(event) {
+    if (event.key === "Escape" || event.key === "Esc") {
+      console.log('Escape');
+      document.querySelectorAll('.modal').forEach(modal => {
+        modal.style.visibility = 'hidden';
+        modal.opacity = '0';
+      })
+    }
+  }
 })(document);
 
+// Hide a modal given an event
 function hideTalkModal(event) {
   event.preventDefault();
   event.target.closest('.modal').style.visibility = 'hidden';
   event.target.closest('.modal').opacity = '0';
 }
 
+// Show a modal given a talk name
 function showTalkModal(event, name) {
   event.preventDefault();
   document.getElementById(name).style.visibility = 'visible';
