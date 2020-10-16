96ddbbf6290c9dad72c552aed24c9abb91e0c2c4
Phil Cluff <578330+GeneticGenesis@users.noreply.github.com>
2020-10-16 15:52:43 +0100

    Speaker and talk descriptions and schedule (#10)
    * Fix up modals for talk descriptions
    
    * Click to dismiss
    
    * Replace modal css with js because just why
    
    * newlines yo
    
    * Cleanup JS
    
    * bye bye css
    
    * Refine modal style a little
    
    * Remove formatting in haml as its now done in the interaction with airtable
    
    * It's time. Adding Schedule and talk descriptions
    
    * Why this underscore needs to be here I do not know
    
    * Make schedule a long list for now
    
    * Curstor types
    
    * Restyle the schedule as we move down responsive breakpoints
    
    * Add a border to the bottom of the modals to stop the talk desc flowing right to the bottom
    
    * Handle escape key to close modals
    
    * Remove JS logging and reinstate link to schedule
    
    * So it turns out that the uglify version in middleman doesn't support => functions. I updated all the dependencies hoping it would fix it, but no. So I replaced it with a normal function. Sigh.
    
    * Add lightning talk submission link
    
    * Small cleanup
    
    * Minor schedule changes
    
    * Nearly forgot timezones!

---
+++ source/javascripts/all.js
@@ -0,0 +1,32 @@
+(function (document) {
+  // Close the open modal when clicked outside
+  window.onclick = function(event) {
+    if (event.target.className == 'modal') {
+      hideTalkModal(event)
+    }
+  }
+
+  // Close all modals on escape key
+  document.onkeydown = function(event) {
+    if (event.key === "Escape" || event.key === "Esc") {
+      document.querySelectorAll('.modal').forEach(function(modal) {
+        modal.style.visibility = 'hidden';
+        modal.opacity = '0';
+      })
+    }
+  }
+})(document);
+
+// Hide a modal given an event
+function hideTalkModal(event) {
+  event.preventDefault();
+  event.target.closest('.modal').style.visibility = 'hidden';
+  event.target.closest('.modal').opacity = '0';
+}
+
+// Show a modal given a talk name
+function showTalkModal(event, name) {
+  event.preventDefault();
+  document.getElementById(name).style.visibility = 'visible';
+  document.getElementById(name).style.opacity = '1';
+}
